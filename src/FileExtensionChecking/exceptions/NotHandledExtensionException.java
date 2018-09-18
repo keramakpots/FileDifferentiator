d7aba6ac398b1d17c99959de10a3e4567a268fc8
Marek Stopka <keramakpots@gmail.com>
2018-09-18 17:21:44 +0200

    Add exception for not handled extensions

---
+++ src/FileExtensionChecking/exceptions/NotHandledExtensionException.java
@@ -0,0 +1,8 @@
+package FileExtensionChecking.exceptions;
+
+public class NotHandledExtensionException extends Exception {
+
+    public NotHandledExtensionException(String message) {
+        super(message);
+    }
+}
