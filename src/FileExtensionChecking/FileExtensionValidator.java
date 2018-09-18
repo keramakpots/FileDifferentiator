dee0984ec6553199e4e5b1c1a0c8b393afd2a1bb
Marek Stopka <keramakpots@gmail.com>
2018-09-18 20:04:49 +0200

    Add exception throwing in comparing extension method

--- src/FileExtensionChecking/FileExtensionValidator.java
+++ src/FileExtensionChecking/FileExtensionValidator.java
@@ -1,6 +1,10 @@
 package FileExtensionChecking;
 
+import FileExtensionChecking.exceptions.NotHandledExtensionException;
+import FileExtensionChecking.exceptions.OtherExtensionException;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 
 public class FileExtensionValidator {
 
@@ -10,9 +14,41 @@ public class FileExtensionValidator {
         this.handledExtensions = handledExtensions;
     }
 
-    boolean checkExtension(byte[] fileBytes, String path) {
+    boolean checkExtension(byte[] fileBytes, String path)
+        throws NotHandledExtensionException, OtherExtensionException {
         String code = getBegginingByteCode(fileBytes);
-        return true;
+        String extensionFromAPath = getExtensionFromPath(path);
+        String extensionExpectedCode = handledExtensions.get(extensionFromAPath);
+        if (extensionExpectedCode == null) {
+            throw new NotHandledExtensionException();
+        } else {
+            if (code.equals(extensionExpectedCode)) {
+                return true;
+            } else {
+                Optional<String> actualExtension = getActualExtension(extensionFromAPath);
+                if (actualExtension.isPresent()) {
+                    throw new OtherExtensionException(actualExtension.get());
+                } else {
+                    throw new NotHandledExtensionException();
+                }
+            }
+        }
+    }
+
+    private Optional<String> getActualExtension(String extensionFromAPath) {
+        return handledExtensions.entrySet()
+            .stream()
+            .filter(entry -> Objects.equals(entry.getValue(), extensionFromAPath))
+            .map(Map.Entry::getKey)
+            .findFirst();
+    }
+
+    private String getExtensionFromPath(String path) {
+        int lastIndexOf = path.lastIndexOf(".");
+        if (lastIndexOf == -1) {
+            return ""; // empty extension
+        }
+        return path.substring(lastIndexOf + 1);
     }
 
     private String getBegginingByteCode(byte[] fileBytes) {
--- src/FileExtensionChecking/exceptions/NotHandledExtensionException.java
+++ src/FileExtensionChecking/exceptions/NotHandledExtensionException.java
@@ -2,7 +2,6 @@ package FileExtensionChecking.exceptions;
 
 public class NotHandledExtensionException extends Exception {
 
-    public NotHandledExtensionException(String message) {
-        super(message);
+    public NotHandledExtensionException() {
     }
 }
