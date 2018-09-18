bd250d29c41503ff273c442ffccc599eeeebc4e5
Marek Stopka <keramakpots@gmail.com>
2018-09-18 17:25:08 +0200

    Add exception for files with extensions which are in fact other extensions

---
+++ src/FileExtensionChecking/exceptions/OtherExtensionException.java
@@ -0,0 +1,8 @@
+package FileExtensionChecking.exceptions;
+
+public class OtherExtensionException extends Exception {
+
+    public OtherExtensionException(String message) {
+        super(message);
+    }
+}
