f2b8bf67b4f833e2eaafb8ec33c15448e7693692
Marek Stopka <keramakpots@gmail.com>
2018-09-18 17:18:06 +0200

    Add fileExtensionValidator Class with constructor

---
+++ src/FileExtensionChecking/FileExtensionValidator.java
@@ -0,0 +1,11 @@
+package FileExtensionChecking;
+
+import java.util.Map;
+
+public class FileExtensionValidator {
+    Map handledExtensions;
+
+    public FileExtensionValidator(Map handledExtensions) {
+        this.handledExtensions = handledExtensions;
+    }
+}
